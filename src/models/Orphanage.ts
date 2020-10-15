ec84a6054788712605314eadc013fd5d833f3e12
willfeliz1 <william_mfelizardo@hotmail.com>
2020-10-15 13:24:12 -0300

    feat: images upload to disk using Multer lib

--- src/models/Orphanage.ts
+++ src/models/Orphanage.ts
@@ -1,4 +1,5 @@
-import { Entity, Column, PrimaryGeneratedColumn} from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany, JoinColumn} from 'typeorm';
+import Image from './Image';
 
 @Entity('orphanages')
 export default class Orphanage {
@@ -28,4 +29,10 @@ export default class Orphanage {
   open_on_weekends: boolean;
 
 
+  @OneToMany(() => Image, image => image.orphanage, {
+    cascade: ['insert', 'update']
+  })
+  @JoinColumn({ name: 'orphanage_id'})
+  images: Image[];
 }
+
