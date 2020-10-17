94cd0abd3019f48196c4def8c001738fc5789790
willfeliz1 <william_mfelizardo@hotmail.com>
2020-10-17 18:00:54 -0300

    feat: nunito google font

--- App.tsx
+++ App.tsx
@@ -2,10 +2,22 @@ import React from 'react';
 import { Dimensions, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import MapView, { Callout, Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import { Feather } from '@expo/vector-icons';
+import { useFonts } from 'expo-font';
+import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold} from '@expo-google-fonts/nunito';
 
 import mapMarker from './src/images/map-marker.png';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Nunito_600SemiBold, 
+    Nunito_700Bold,
+    Nunito_800ExtraBold
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <MapView 
@@ -70,6 +82,7 @@ const styles = StyleSheet.create({
   calloutText: {
     color: '#95c4fc',
     fontSize: 14,
+    fontFamily: 'Nunito_700Bold',
   },
 
   footer: {
@@ -91,6 +104,7 @@ const styles = StyleSheet.create({
   },
 
   footerText: {
+    fontFamily: 'Nunito_700Bold',
     color: '#8fa7b3',
   },
 
