f9d435eeeed5fc2400ec92a881e1ffdf1b4f3609
willfeliz1 <william_mfelizardo@hotmail.com>
2020-10-17 15:12:50 -0300

    fix: return as boolean

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -53,7 +53,7 @@ export default {
       about,
       instructions,
       opening_hours,
-      open_on_weekends,
+      open_on_weekends: open_on_weekends === 'true',
       images,
     };
 
@@ -71,6 +71,7 @@ export default {
         })
       )
     });
+
 
     await schema.validate(data, {
       abortEarly: false,
