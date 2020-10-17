e6bfd9332c2908d6b96c359e09ae455fcbae207b
willfeliz1 <william_mfelizardo@hotmail.com>
2020-10-17 15:28:43 -0300

    fix: name incorrect

--- src/pages/Orphanage.tsx
+++ src/pages/Orphanage.tsx
@@ -17,7 +17,7 @@ interface Orphanage {
   about: string;
   instructions: string;
   opening_hours: string;
-  open_on_weeken: string;
+  open_on_weekends: string;
   images: Array<{
     id: number;
     url: string;
@@ -108,7 +108,7 @@ export default function Orphanage() {
                 {orphanage.opening_hours}
               </div>
               
-              { orphanage.open_on_weeken ? (
+              { orphanage.open_on_weekends ? (
                 <div className="open-on-weekends">
                 <FiInfo size={32} color="#39CC83" />
                 Atendemos <br />
